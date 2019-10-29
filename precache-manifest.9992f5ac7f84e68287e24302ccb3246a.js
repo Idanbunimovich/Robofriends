4ee44443e7e218c2c399ea70a1ca3880dd0d0d7f
Idanbunimovich <56599359+Idanbunimovich@users.noreply.github.com>
2019-10-29 12:52:05 +0200

    Updates

---
+++ precache-manifest.9992f5ac7f84e68287e24302ccb3246a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcb61d98999d50a7b50631e19bce082",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cec90a06851b32c72799",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "63dfae84f0d16571d342",
+    "url": "/robofriends/static/css/main.526d9342.chunk.css"
+  },
+  {
+    "revision": "cec90a06851b32c72799",
+    "url": "/robofriends/static/js/2.67098666.chunk.js"
+  },
+  {
+    "revision": "63dfae84f0d16571d342",
+    "url": "/robofriends/static/js/main.611b5221.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
